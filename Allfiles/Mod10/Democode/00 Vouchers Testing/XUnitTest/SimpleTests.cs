babc14198b2c9d2a564fdfb9c852884c2b4c2933
Alexander Pajer <alexander.pajer@integrations.at>
2019-04-09 10:17:07 +0200

    xUnit Test added

---
+++ Allfiles/Mod10/Democode/00 Vouchers Testing/XUnitTest/SimpleTests.cs
@@ -0,0 +1,29 @@
+﻿using Vouchers.Common;
+using Xunit;
+
+namespace VouchersTests
+{
+    public class CalculationTests
+    {
+        [Fact]
+        public void TestEquality()
+        {
+            Assert.Equal(4, Calculation.Add(2, 2));
+        }
+
+        [Fact]
+        public void TestForEqual()
+        {
+            Assert.NotEqual(5, Calculation.Add(2, 2));
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void IsOddTheory(int value)
+        {
+            Assert.True(Calculation.IsOdd(value));
+        }        
+    }
+}
