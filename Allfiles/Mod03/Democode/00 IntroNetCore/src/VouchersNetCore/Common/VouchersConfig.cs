5d808a54a82829ec242537ed4658bc3bbf15f71a
Alexander Pajer <alexander.pajer@integrations.at>
2019-04-23 08:09:56 +0200

    update

---
+++ Allfiles/Mod03/Democode/00 IntroNetCore/src/VouchersNetCore/Common/VouchersConfig.cs
@@ -0,0 +1,27 @@
+﻿namespace Vouchers
+{
+    //http://json2csharp.com/ -> json to c# generieren
+    public class VouchersConfig
+    {
+        public ConnectionStrings ConnectionStrings { get; set; }
+        public string ApplicationName { get; set; }
+        public Authentication Authentication { get; set; }
+    }
+
+    public class ConnectionStrings
+    {
+        public string LocalDBConnection { get; set; }
+        public string SQLServerDBConnection { get; set; }
+    }
+
+    public class Facebook
+    {
+        public string AppId { get; set; }
+        public string AppSecret { get; set; }
+    }
+
+    public class Authentication
+    {
+        public Facebook Facebook { get; set; }
+    }
+}
